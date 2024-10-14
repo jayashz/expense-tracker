9a9343e2f6959c50c136b5c14df1054909d7ea68
jayashz <jayzstha15@gmail.com>
2024-10-14 16:07:32 +0545

    Feat:added calender for date input

--- component/Expenses/ManageExp/ExpenseForm.js
+++ component/Expenses/ManageExp/ExpenseForm.js
@@ -1,30 +1,28 @@
 import React, { useState } from "react";
-import { Button, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Input from "./Input";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import CustomBtn from "../../ui/CustomBtn";
 import { Platform } from "react-native";
+import { colors } from "../../../constants/Colors";
 const ExpenseForm = () => {
   const [date, setDate] = useState(new Date());
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
-  function priceInputHandler() {
-    
-  }
-  function titleInputHandler(){
-
-  }
-  function onDateChange(e, selectedDate){
+  function priceInputHandler() {}
+  function titleInputHandler() {}
+  function onDateChange(e, selectedDate) {
     setDate(selectedDate);
+    setOpen(false);
   }
-  
 
   return (
     <View>
       <Input
         label="Title"
         inputConfig={{
-          onChangeText: titleInputHandler
+          onChangeText: titleInputHandler,
+          placeholder: "Title",
         }}
       />
       <Input
@@ -32,15 +30,36 @@ const ExpenseForm = () => {
         inputConfig={{
           keyboardType: "number-pad",
           onChangeText: priceInputHandler,
+          placeholder: "Price",
         }}
       />
-      {Platform.OS == 'android'?<CustomBtn onPress={() => setOpen(!open)}>{date.toDateString()}</CustomBtn> : ''}
-      <DateTimePicker 
-      value={date}
-      onChange={onDateChange}
-      />
+        <View style={styles.innerContainer}>
+      {Platform.OS == "android" ? (
+        <CustomBtn style={styles.cusBtn} onPress={() => setOpen(!open)}>
+          {date.toDateString()}
+        </CustomBtn>
+      ) : (
+        
+            <DateTimePicker value={date} onChange={onDateChange} />
+        
+      )}
+
+      {open && <DateTimePicker value={date} onChange={onDateChange} />}
+      </View>
+      
     </View>
   );
 };
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({
+  cusBtn: {
+    marginTop: 20,
+  },
+  innerContainer:{
+    justifyContent:'center',
+    alignItems:'center',
+    marginVertical:10
+  }
+});
--- component/Expenses/ManageExp/Input.js
+++ component/Expenses/ManageExp/Input.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { colors } from "../../../constants/Colors";
 
-const Input = ({label,inputConfig}) => {
+const Input = ({inputConfig}) => {
   return (
-    <View>
-        <Text>{label}</Text>
+    <View style={styles.container}>
       <TextInput {...inputConfig} />
     </View>
   );
 };
 
 export default Input;
+
+const styles= StyleSheet.create({
+    container:{
+        backgroundColor:colors.primary100,
+        marginVertical:10,
+        padding:5,
+        borderRadius:5
+    }
+});
 
