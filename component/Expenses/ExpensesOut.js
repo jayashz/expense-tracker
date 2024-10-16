5b7616695bcec605b27f7e3e0025e2a7f34f0df1
jayashz <jayzstha15@gmail.com>
2024-10-16 12:08:35 +0545

    Refactor

--- component/Expenses/ExpensesOut.js
+++ component/Expenses/ExpensesOut.js
@@ -4,22 +4,16 @@ import ExpensesSumm from "./ExpensesSumm";
 import ExpensesList from "./ExpList/ExpensesList";
 import { colors } from "../../constants/Colors";
 import { useSelector } from "react-redux";
-import { fetchExp } from "../../util/https";
-import { setExpense } from "../../store/exp-slice";
-import { useDispatch } from "react-redux";
-import LoadingOverlay from "../ui/LoadingOverlay";
-import ErrorScreen from "../ui/ErrorScreen";
+
+
 const ExpensesOut = ({ expPeriod }) => {
-  const dispatch = useDispatch();
   const data = useSelector((state) => state.expenses.exp);
-  const [isFetching,setIsFetching]=useState(true);
-  const [error,setError] = useState();
+
   const presentDate = new Date();
   const presentDay = presentDate.getDate();
   const presentMonth = presentDate.getMonth() + 1;
   const presentYear = presentDate.getFullYear();
   
-
   const filteredData = data.filter((exp) => {
     const expDate = new Date(exp.date);
     const expYear = expDate.getFullYear();
@@ -37,28 +31,7 @@ const ExpensesOut = ({ expPeriod }) => {
     
   });
 
-  useEffect(() => {
-    async function getExpenses() {
-      setIsFetching(true);
-      try{
-        const expenses = await fetchExp();
-        dispatch(setExpense(expenses));
-      }catch(error){
-        setError('Could not fetch Expenses...');
-      }
-      setIsFetching(false);
-    }
-    getExpenses();
-  }, []);
-  function errorHandler(){
-    setError(null);
-  }
-  if(error){
-    return <ErrorScreen message={error} onConfirm={errorHandler} />
-  }
-  if(isFetching){
-    return <LoadingOverlay/>
-  }
+ 
   return (
     <View style={styles.mainContainer}>
       <ExpensesSumm
--- screens/RecentScreen.js
+++ screens/RecentScreen.js
@@ -1,8 +1,40 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React,{useState, useEffect} from 'react'
 import ExpensesOut from '../component/Expenses/ExpensesOut'
+import { fetchExp } from '../util/https';
+import { setExpense } from '../store/exp-slice';
+import LoadingOverlay from '../component/ui/LoadingOverlay';
+import ErrorScreen from '../component/ui/ErrorScreen'
+import { useDispatch } from "react-redux";
 
 const RecentScreen = () => {
+  const dispatch = useDispatch();
+  const [isFetching,setIsFetching]=useState(true);
+  const [error,setError] = useState();
+
+  useEffect(() => {
+    async function getExpenses() {
+      setIsFetching(true);
+      try{
+        const expenses = await fetchExp();
+        dispatch(setExpense(expenses));
+      }catch(error){
+        setError('Could not fetch Expenses...');
+      }
+      setIsFetching(false);
+    }
+    getExpenses();
+  }, []);
+
+  function errorHandler(){
+    setError(null);
+  }
+  if(error){
+    return <ErrorScreen message={error} onConfirm={errorHandler} />
+  }
+  if(isFetching){
+    return <LoadingOverlay/>
+  }
+
   return (
     <ExpensesOut expPeriod='Last 7 days'/>
   )
